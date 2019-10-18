9023ac1903cfff487b7e8c6d30a7116cdb1c33e3
unknown <l13854867789@163.com>
2019-10-18 22:34:08 +0800

    Signed-off-by: unknown <l13854867789@163.com>

--- 第三章作业/3.2/习题2.cpp
+++ 第三章作业/3.2/习题2.cpp
@@ -1,40 +1,14 @@
 #include<stdio.h>
 #include<math.h>
-//#include<stdlib.h>
 int main()
 {
-	float r1=0.015,r2=0.021,r3=0.0275,r5=0.03,rh=0.0035,p;
-	int x,n=5;
-	printf("请选择方案：\n");
-	scanf("%d",&x);
-	if(x==1)
-	{
-		p=1000*(1+n*r5);
-		printf("本息和为： %f\n",p);
-	}
-	else if(x==2)
-	{
-		p=1000*(1+2*r2)*(1+3*r3);
-		printf("本息和为： %f\n",p);
-	}
-	else if(x==3)
-	{
-		p=1000*(1+3*r3)*(1+2*r2);
-		printf("本息和为： %f\n",p);
-	}
-	else if(x==4)
-	{
-        p=1000;
-        p=p*pow(1+r1,n);
-		printf("本息和为： %f\n",p);
-	}
-	else if(x==5)
-	{
-        p=1000;
-	    p=p*pow(1+rh/4,4*n);
-		printf("本息和为： %f\n",p);
-	}
-	else printf("无对应方案\n");
-//	system("pause");
+	float r1=0.015,r2=0.021,r3=0.0275,r5=0.03,rh=0.0035,p1,p2,p3,p4,p5;
+	int x,n1=5,n2=2,n3=3;
+	p1=1000*(1+n1*r5);
+	p2=1000*(1+n2*r2)*(1+n3*r3);
+	p3=1000*(1+n3*r3)*(1+n2*r2);
+    p4=1000*pow(1+r1,n1);
+	p5=1000*pow(1+rh/4,4*n1);
+    printf("方案一：%f\n方案二：%f\n方案三：%f\n方案四：%f\n方案五：%f\n",p1,p2,p3,p4,p5);
 	return 0;
 }
